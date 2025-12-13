ee178a3c82a53b7d44380a2e6b3508cc8519362d
Fox-Demon-605 <slava-vasilev-2005@bk.ru>
2025-12-13 10:48:44 +0300

    Add files via upload

--- frontend/script.js
+++ frontend/script.js
@@ -1,24 +1,24 @@
 
-const ws = new WebSocket("ws://localhost:8000/ws/chat?token=TEST&session_id=1");
-const box = document.getElementById("messages");
-const typing = document.getElementById("typing");
-const input = document.getElementById("input");
+const ws = new WebSocket("wss://YOUR_DOMAIN/ws/chat?token=JWT&session_id=1");
+const box=document.getElementById("messages");
+const typing=document.getElementById("typing");
+const input=document.getElementById("input");
 
-ws.onmessage = e => {
-  typing.style.display="none";
-  const d = JSON.parse(e.data);
-  box.innerHTML += `<div>${d.text}</div>`;
+ws.onmessage=e=>{
+ typing.style.display="none";
+ const d=JSON.parse(e.data);
+ box.innerHTML+=`<div>${d.text}</div>`;
 };
 
-document.getElementById("send").onclick = send;
-input.onkeydown = e => e.key==="Enter" && send();
+document.getElementById("send").onclick=send;
+input.onkeydown=e=>e.key==="Enter"&&send();
 
 function send(){
-  if(!input.value) return;
-  box.innerHTML += `<div><b>Вы:</b> ${input.value}</div>`;
-  ws.send(JSON.stringify({message:input.value}));
-  input.value="";
-  typing.style.display="block";
+ if(!input.value)return;
+ box.innerHTML+=`<div><b>Вы:</b> ${input.value}</div>`;
+ ws.send(JSON.stringify({message:input.value}));
+ input.value="";
+ typing.style.display="block";
 }
 
-document.getElementById("clear").onclick = ()=> box.innerHTML="";
+document.getElementById("clear").onclick=()=>box.innerHTML="";
